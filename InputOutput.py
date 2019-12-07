d9f6e15295de7da30e7529e1e24c6610383fb186
PVegananda <sherlock.redhat@gmail.com>
2019-12-07 21:41:36 +0700

    commit kedua

---
+++ InputOutput.py
@@ -0,0 +1,20 @@
+"""
+    Input Output dalam Bahasa Python
+    Date    : Saturday, 7/12/2019
+    Author  : Pasyah Vegananda
+    GitHub  : PVegananda
+    
+"""
+
+print(" Output Print ") #Output menggunakan print
+
+"""
+
+"""
+
+var_1 = 5                    #Membuat Variabel var_1
+print("The Value is", var_1) #Memberikan Output nilai value didalam var_1
+
+"""
+
+"""
